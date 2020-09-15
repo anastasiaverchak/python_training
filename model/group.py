8daf13c10887b663067bec71f18869eafd4125ce
Anastasia Verchak <anastasia.verchak@cleverlance.com>
2020-09-15 18:51:30 +0300

    asserts errors

--- model/group.py
+++ model/group.py
@@ -11,10 +11,14 @@ class Group:
 
 
     def __repr__(self):
-        return "%s:%s" % (self.id,self.name)
+        return "%s:%s;%s;%s" % (self.id,self.name, self.header, self.footer)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id) and (
+                self.name == other.name or self.name == None or other.name == None) and (
+            self.header is None or other.header is None or self.header == other.header) and (
+            self.footer is None or other.footer is None or self.footer == other.footer
+        )
 
     def id_or_max(self):
         if self.id:
