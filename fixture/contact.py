adef348526062479555994d1e9341203c68161e7
Anastasia Verchak <anastasia.verchak@cleverlance.com>
2020-09-15 16:53:42 +0300

    task 14

--- fixture/contact.py
+++ fixture/contact.py
@@ -2,6 +2,9 @@ from selenium.webdriver.support.ui import Select
 
 from model.contact import Contact
 
+import re
+
+
 class ContactHelper:
     def __init__(self, app):
         self.app = app
@@ -101,12 +104,63 @@ class ContactHelper:
     def get_contact_list(self):
         if self.contacts_cache is None:
             wd = self.app.wd
-            self.open_home_page()
+            self.app.open_home_page()
             self.contacts_cache = []
             for element in wd.find_elements_by_xpath("//tr[contains(@name,'entry')]"):
                 cells = element.find_elements_by_tag_name("td")
                 lastname = cells[1].text
                 firstname = cells[2].text
                 id = element.find_element_by_name("selected[]").get_attribute("value")
-                self.contacts_cache.append(Contact(lastname = lastname, firstname= firstname, id = id))
+                address = cells[3].text
+                emails = cells[4].text
+                all_phones = cells[5].text
+                self.contacts_cache.append(Contact(lastname = lastname, firstname= firstname, id = id,
+                                                   all_phones_from_homepage = all_phones, emails =emails, address = address))
         return list(self.contacts_cache)
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+
+        homephone = wd.find_element_by_name("home").get_attribute("value")
+        workphone = wd.find_element_by_name("work").get_attribute("value")
+        mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
+        secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
+
+        address = wd.find_element_by_name("address").get_attribute("value")
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
+
+        return Contact(firstname = firstname, lastname =lastname, id =id, home =homephone, work = workphone,
+                       mobile = mobilephone, phone2 =secondaryphone, address = address,
+                       email = email, email2 = email2, email3 = email3)
+
+    def get_contact_info_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        secondaryphone = re.search("P: (.*)", text).group(1)
+        return Contact(home =homephone, work = workphone,
+                       mobile = mobilephone, phone2 =secondaryphone)
+
--- model/contact.py
+++ model/contact.py
@@ -6,7 +6,7 @@ class Contact:
     def __init__(self, firstname = None, middlename = None, lastname = None, nickname = None, title = None, company = None,
                  address= None, home = None, mobile = None, work = None, fax = None, email = None, email2 = None, email3 = None,
                  homepage = None, bday = None, bmonth = None, byear = None, aday = None, amonth = None, ayear = None,
-                 address2 = None, phone2 = None, notes = None, id = None):
+                 address2 = None, phone2 = None, notes = None, id = None, all_phones_from_homepage = None, emails = None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -32,7 +32,8 @@ class Contact:
         self.phone2 = phone2
         self.notes = notes
         self.id = id
-
+        self.all_phones_from_homepage = all_phones_from_homepage
+        self.emails = emails
 
     def __repr__(self):
         return "%s:%s" % (self.id,self.lastname)
