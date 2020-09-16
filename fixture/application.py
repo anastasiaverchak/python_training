0de72c585737f1caa5c424185ef6b4b3070daa5e
Anastasia Verchak <anastasia.verchak@cleverlance.com>
2020-09-16 15:50:34 +0300

    task 16

--- fixture/application.py
+++ fixture/application.py
@@ -6,12 +6,22 @@ from fixture.session import SessionHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "opera":
+            self.wd = webdriver.Opera()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(2)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,7 +33,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_xpath("//form[@name='MainForm']//div[1]//input[1]")) >0):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
 
     def destroy(self):
